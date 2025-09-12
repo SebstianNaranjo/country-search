1c5383b6493a5e0f3aa60576069bbc860f31ae5f
SebastiaNaranjo <js.naranjo30@ciaf.edu.co>
2025-09-11 23:13:48 -0500

    se llamas los datos de la api en countrycard

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
 
   return (
     <>
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,6 +1,23 @@
 import PropTypes from "prop-types";
 
 export const CountryCard = ({ country }) => {
+    return (
+        <div className="country-card">
+            <img src={country.flag} alt={`Flag of ${country.name}`} className="country-flag" />
+            <h2 className="country-name">{country.name}</h2>
+            <p><strong>Capital:</strong> {country.capital}</p>
+            <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+            <p><strong>Region:</strong> {country.region}</p>
+        </div>
+    );
+};
 
-    
-}
+CountryCard.propTypes = {
+    country: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        capital: PropTypes.string.isRequired,
+        flag: PropTypes.string.isRequired,
+        population: PropTypes.number.isRequired,
+        region: PropTypes.string.isRequired,
+    }).isRequired,
+};
