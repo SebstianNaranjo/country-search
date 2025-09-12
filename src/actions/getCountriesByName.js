733d115c3c19663c14827d9abf908253d1bf3886
SebastiaNaranjo <js.naranjo30@ciaf.edu.co>
2025-09-11 23:02:01 -0500

    se actualiza datos de la api

--- src/actions/getCountriesByName.js
+++ src/actions/getCountriesByName.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 
 export const getCountriesByName = async (query) => {
-  const url = `https://restcountries.com/v3.1/name/${query}`;
-  const response = await axios.get(url);
-  return response.data;
+  const url = "https://restcountries.com/v3.1/name/" + query;
+  const response = await axios.get(url, {
+    params: {
+      fields: "name,capital,flags,population,region"
+    }
+  });
+
+  const data = response.data;
+
+  const responseMap = data.map((country) => ({
+    name: country.name.common,
+    capital: country.capital ? country.capital[0] : "Sin capital",
+    flag: country.flags.svg,
+    population: country.population,
+    region: country.region,
+  }));
+
+  return responseMap;
 };
