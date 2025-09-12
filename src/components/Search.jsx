33779985690d5ad3f2ef7ae4e8786a71ff8e3906
SebastiaNaranjo <js.naranjo30@ciaf.edu.co>
2025-09-11 22:52:11 -0500

    se implementa search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,3 +1,39 @@
 export const Search = ({ placeholder, onSearch }) => {
-    
-}
+  const [search, setSearch] = useState("");
+
+  const handleChange = (evento) => {
+    setSearch(evento.target.value);
+  };
+
+  const handleSubmit = (evento) => {
+    evento.preventDefault();
+    onSearch(search);
+    if (search.trim() === "") return;
+
+    onSearch(search);
+    setSearch("");
+  };
+
+  const handleKeyDown = (evento) => {
+    if (evento.key === "Enter") {
+      handleSubmit(evento);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className="content-center">
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={search}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button type="submit">Buscar</button>
+    </form>
+  );
+};
+
+Search.propTypes = {
+  placeholder: PropTypes.string,
+  onSearch: PropTypes.func.isRequired,
+};
